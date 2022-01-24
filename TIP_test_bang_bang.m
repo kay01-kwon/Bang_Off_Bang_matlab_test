ce9960d104136bbe002162715295aa3f18a8faf8
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2022-01-25 00:09:04 +0900

    Add files via upload

--- TIP_test_bang_bang.m
+++ TIP_test_bang_bang.m
@@ -2,15 +2,15 @@ clear all;
 close all;
 
 dt = 0.01;
-tf = 1;
+tf = 100;
 t = 0:dt:tf;
 
 N = length(t);
 
 % Unit: m/s
-j_max = 0.01;
-a_max = 0.1;
-v_max = 0.6;
+j_max = 0.001;
+a_max = 0.01;
+v_max = 0.05;
 
 j = zeros(1,length(t));
 a = zeros(1,length(t));
@@ -22,14 +22,37 @@ a_sum = zeros(1,length(t));
 v_sum = zeros(1,length(t));
 p_sum = zeros(1,length(t));
 
+goal_distance = 1;
 
-t01 = sqrt(v_max/j_max);
-t01=j_max/a_max;
+t01 = a_max/j_max;
+t02 = v_max/a_max -t01;
 
-t1 = t01;
-t2 = 2*t01;
+if t02 <= 0
+    t01 = sqrt(v_max/j_max);
+    t1 = t01;
+    t02 = t1;
+    t2 = 2*t01;
+end
 
-for i = 1:N-1
+a_t1 = j_max*t1;
+v_t1 = 1/2*j_max*t1^2;
+p_t1 = 1/6*j_max*t1^3;
+
+v_t2 = -j_max*t01 + a_t1*t01;
+p_t2 = -j_max/6*t1^3 + a_t1/2*t01^2 + v_t1*t01 + p_t1;
+
+p_stop_start = goal_distance - p_t2;
+
+a_t3 = -j_max*t01;
+v_t3 = -j_max/2*t01^2 + v_max;
+p_t3 = -j_max/6*t01^3 + v_max*t01 + p_stop_start;
+
+
+t_stop_start = (goal_distance - 2*p_t2)/v_max + t2;
+t3 = t_stop_start + t01;
+t4 = t3 + t01;
+
+for i = 1:N
     
     if t(i)>=0 && t(i)<t1
         j(i) = j_max;
@@ -37,65 +60,62 @@ for i = 1:N-1
         v(i) = 1/2*j_max*t(i)^2;
         p(i) = 1/6*j_max*t(i)^3;
         
-        j_sum(i) = j_max;
-        a_sum(i+1) = a_sum(i) + j_sum(i)*dt;
-        v_sum(i+1) = v_sum(i) + a_sum(i)*dt;
-        p_sum(i+1) = p_sum(i) + v_sum(i)*dt;
-        
-    elseif t(i)>=t1 && t(i)<t2
+    elseif t(i)>=t1 && t(i) < t2
         j(i) = -j_max;
-        a(i) = -j_max*(t(i)-t01)+j_max*t01;
-        v(i) = -j_max/2*t(i)*(t(i)-2*t01)+j_max*t01*t(i)-j_max*t01^2;
-        p(i) = -j_max/2*(1/3*t(i)^3-t01*t(i)^2)+j_max/2*t01*t(i)^2-j_max*t01^2*t(i)+1/3*j_max*t01^3;
-        
-        p_t2 = j_max*t01^3;
+        a(i) = -j_max*(t(i)-t1) + a_t1;
+        v(i) = -j_max/2*(t(i)-t1)^2 + a_t1*(t(i)-t1) + v_t1;
+        p(i) = -j_max/6*(t(i)-t1)^3 + a_t1/2*(t(i)-t1)^2 + v_t1*(t(i)-t1) + p_t1;
         
         j_sum(i) = -j_max;
         a_sum(i+1) = a_sum(i) + j_sum(i)*dt;
         v_sum(i+1) = v_sum(i) + a_sum(i)*dt;
         p_sum(i+1) = p_sum(i) + v_sum(i)*dt;
+        
     elseif t(i)>=t2
         j(i) = 0;
         a(i) = 0;
         v(i) = v_max;
-        p(i) = p_t2+v(i)*(t(i)-t2);
-        
+        p(i) = p_t2 + v_max*(t(i)-t2);
         
         j_sum(i) = 0;
         a_sum(i+1) = 0;
         v_sum(i+1) = v_max;
         p_sum(i+1) = p_sum(i) + v_sum(i)*dt;
+    end
+    
+    if t(i) >= t_stop_start && t(i) < t3
+        j(i) = -j_max;
+        a(i) = -j_max*(t(i)-t_stop_start);
+        v(i) = -j_max/2*(t(i)-t_stop_start)^2 + v_max;
+        p(i) = -j_max/6*(t(i)-t_stop_start)^3 + v_max*(t(i)-t_stop_start) + p_stop_start;
         
+    elseif t(i) >= t3 && t(i) < t4
+        j(i) = j_max;
+        a(i) = j_max*(t(i)-t3) + a_t3;
+        v(i) = j_max/2*(t(i)-t3)^2 + a_t3*(t(i)-t3) + v_t3;
+        p(i) = j_max/6*(t(i)-t3)^3 + a_t3/2*(t(i)-t3)^2 + v_t3*(t(i)-t3) + p_t3;
+        
+    elseif t(i) >=t4
+        j(i) = 0;
+        a(i) = 0;
+        v(i) = 0;
+        p(i) = goal_distance;
     end
 end
 
-v(end) = v(N-1);
-p(end) = p(N-1);
 
 subplot(2,2,1)
 plot(t,j);
-hold on;
-plot(t,j_sum);
 title('Jerk')
-legend('Numerical','Sum')
 
 subplot(2,2,2)
 plot(t,a);
-hold on;
-plot(t,a_sum);
 title('Acc')
-legend('Numerical','Sum')
 
 subplot(2,2,3)
 plot(t,v);
-hold on;
-plot(t,v_sum);
 title('Vel')
-legend('Numerical','Sum')
 
 subplot(2,2,4)
 plot(t,p);
-hold on;
-plot(t,p_sum);
 title('Pos')
-legend('Numerical','Sum')
